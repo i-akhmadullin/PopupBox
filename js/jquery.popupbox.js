96f3a66414d9df304885e09c792d748b0900e0af
ilya.akh <rafoex@gmail.com>
2012-09-01 02:30:52 +0600

    before&after actions with callbacks

--- js/jquery.popupbox.js
+++ js/jquery.popupbox.js
@@ -1,19 +1,24 @@
+/*  $("selector").popupbox()
+	then
+	$("selector").popupbox("open"),
+	$("selector").popupbox("close")
+*/
 ;(function ($) {
 	$.popupbox = function(el, options) {
 		var $doc  = $(document);
 		var $body = $('body');
 		var base  = this, o; // o - настройки
 		base.$el  = $(el);
-		base.$el.data("link", base);
+		base.$el.data('popupbox', base);
 		base.isOpen = false;
 
 		base.options = o = $.extend({},$.popupbox.defaults, options);
 
-		base.$wnd     = $("<div class='" + o.windowClass  + "'></div>");
-		base.$close   = $("<a class='"   + o.closeClass   + "' href='#'></a>");
-		base.$popup   = $("<div class='" + o.popupClass   + "'></div>");
-		base.$helper  = $("<div class='" + o.helperClass  + "'></div>");
-		base.$overlay = $("<div class='" + o.overlayClass + "'></div>");
+		base.$wnd     = $('<div class="' + o.windowClass  + '"></div>');
+		base.$close   = $('<a class="'   + o.closeClass   + '" href="#"></a>');
+		base.$popup   = $('<div class="' + o.popupClass   + '"></div>');
+		base.$helper  = $('<div class="' + o.helperClass  + '"></div>');
+		base.$overlay = $('<div class="' + o.overlayClass + '"></div>');
 
 		base.build = function() {
 			base.$close.appendTo(base.$wnd);
@@ -22,8 +27,8 @@
 				base.$popup.prepend(base.$overlay);
 			}
 
-			var contentUrl = base.$el.attr("href");
-			base.content = $(contentUrl).clone(true,true);
+			var contentUrl = base.$el.attr('href');
+			base.content = $(contentUrl ? contentUrl : base.$el).clone(true,true);
 			base.content.appendTo(base.$wnd).show();
 		};
 
@@ -35,25 +40,32 @@
 		};
 
 		base.open = function() {
-			$body.append(base.$popup);
-
-			base.$overlay.show();
-			base.$popup.show();
-			base.isOpen = true;
+			var callback = function() {
+				$body.append(base.$popup);
+				base.$overlay.show();
+				base.$popup.show();
+				base.isOpen = true;
+				execFunction('afterShow');
+			};
+			execFunction('beforeShow', callback);
 		};
 
 		base.close = function() {
-			base.$overlay.hide();
-			base.$popup.hide();
-			base.isOpen = false;
+			var callback = function() {
+				base.$overlay.hide();
+				base.$popup.hide();
+				base.isOpen = false;
+				execFunction('afterClose');
+			};
+			execFunction('beforeClose', callback);
 		};
 
 		base.addClickListener = function() {
-			$doc.on('click', "." + o.closeClass + "," + "." + o.popupClass, function() {
+			$doc.on('click', '.' + o.closeClass + ',' + '.' + o.popupClass, function() {
 				base.close();
 				return false;
 			});
-			$doc.on('click', "." + o.windowClass, function() {
+			$doc.on('click', '.' + o.windowClass, function() {
 				return false;
 			});
 			base.$el.click(function(e) {
@@ -73,29 +85,41 @@
 		};
 
 		base.init();
+
+		function execFunction(funct, cb) {
+			var f = o[funct];
+			if (f && typeof(f) == 'function') {
+				f(base, cb);
+			} else if (cb) {
+				cb();
+			}
+		}
 	};
 
 	$.popupbox.defaults = {
-		popupClass:   "b-popupbox",
-		closeClass:   "b-popupbox__close",
-		helperClass:  "b-popupbox__helper",
-		windowClass:  "b-popupbox__window",
-		overlayClass: "b-popupbox__overlay",
-		overlay: true
+		popupClass:   'b-popupbox',
+		closeClass:   'b-popupbox__close',
+		helperClass:  'b-popupbox__helper',
+		windowClass:  'b-popupbox__window',
+		overlayClass: 'b-popupbox__overlay',
+		overlay:      true,
+		beforeShow:   false,
+		afterShow:    false,
+		beforeClose:  false,
+		afterClose:   false
 	};
 	$.fn.popupbox = function(options) {
 		if ((typeof(options)).match('object|undefined')) {
 			return this.each(function() {
 				(new $.popupbox(this, options));
 			});
-		} /*else if (/\d/.test(options) && !isNaN(options)) {
+		} else if ('open' === options || 'close' === options) {
 			return this.each(function() {
-				var found_popupbox = $.data(this, 'link');
-				if (found_popupbox) {
-					// var page = (typeof(options) == "number") ? options : parseInt($.trim(options), 10);
-					// found_popupbox.gotoPage(page);
+				var popupbox = $.data(this, 'popupbox');
+				if (popupbox) {
+					popupbox[options]();
 				}
 			});
-		}*/
+		}
 	};
 })(jQuery);
